2f29bb4bfae1c9551323471853c67dd4e3882e4a
Dongping Zhang <dpzhang@berkeley.edu>
2017-03-14 01:46:00 -0500

    added phantomJS installation to alda_prep.sh

--- alda_prep.sh
+++ alda_prep.sh
@@ -8,5 +8,14 @@ sudo python3 -m pip install Django==1.10.6
 sudo python3 -m pip install selenium
 sudo python3 -m pip install numpy
 sudo python3 -m pip install openpyxl
+
+# install PhantomJS
+sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+# extract the files to directory
+sudo tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+
+# move files to Phantom's directory
+sudo ln -s /usr/local/share/phantomjs-1.9.0-linux-x8664/bin/phantomjs /usr/local/share/phantomjs; sudo ln -s /usr/local/share/phantomjs-1.9.0-linux-x8664/bin/phantomjs /usr/local/bin/phantomjs; sudo ln -s /usr/local/share/phantomjs-1.9.0-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+
 cd AldaCourse/aldacourse/
 python3 manage.py runserver
